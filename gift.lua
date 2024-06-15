e60382b620b6a2253647d71d1667be74d0538ea2
Chen2005 <52918237+Chen2005@users.noreply.github.com>
2024-06-15 21:12:16 +0100

    Update gift.lua

--- gift.lua
+++ gift.lua
@@ -1,7 +1,7 @@
 local Data = require(game.ReplicatedStorage.ClientModules.Core.ClientData) 
 local localPlayer = game.Players.LocalPlayer
 local Fsys = require(game.ReplicatedStorage:WaitForChild("Fsys")).load
-local rawAmount = game:GetService("Players").LocalPlayer.PlayerGui.TicketsIndicatorApp.CurrencyIndicator.Container.Amount.Text
+local rawAmount = game:GetService("Players").LocalPlayer.PlayerGui.TicketIndicatorApp.CurrencyIndicator.Container.Amount.Text
 local amount = rawAmount:gsub(",", "")  -- Remove commas from the raw amount
 local Counter = 0
 local gui = Instance.new("ScreenGui")
